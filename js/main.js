bbd20470be930fb86e58f9813fcca8f38f51783c
RidoyHossan1 <ridoyhossanrony2006@gmail.com>
2023-07-12 11:21:44 +0600

    update

---
+++ js/main.js
@@ -0,0 +1,48 @@
+document.addEventListener("DOMContentLoaded", function () {
+  let span = document.getElementsByTagName("span");
+  let product = document.getElementsByClassName("product");
+  let product_page = Math.ceil(product.length / 4);
+  let l = 0;
+  let movePer = 25.34;
+  let MaxMove = 203;
+
+  // Mobile View
+  let mob_view = window.matchMedia("(max-width: 768px)");
+  if (mob_view.matches) {
+    movePer = 50.36;
+    MaxMove = 504;
+  }
+
+  let right_mover = () => {
+    l = l + movePer;
+    if (product.length == 1) {
+      l = 0;
+    }
+    for (const i of product) {
+      if (l > MaxMove) {
+        l = l - movePer;
+      }
+      i.style.left = "-" + l + "%";
+    }
+  };
+
+  let left_mover = () => {
+    l = l - movePer;
+    if (l <= 0) {
+      l = 0;
+    }
+    for (const i of product) {
+      if (product_page > 1) {
+        i.style.left = "-" + l + "%";
+      }
+    }
+  };
+
+  span[1].onclick = () => {
+    right_mover();
+  };
+
+  span[0].onclick = () => {
+    left_mover();
+  };
+});
